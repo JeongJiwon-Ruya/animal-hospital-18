751dafb93e8d6ad1963cfeb95edf62a209b25f8d
JeongJiwon-Ruya <eetr3295@gmail.com>
2021-04-03 19:37:52 +0900

    Initial commit

---
+++ src/main/java/org/cnu/realcoding/animalhospital18/AnimalHospital18Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.animalhospital18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHospital18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalHospital18Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/cnu/realcoding/animalhospital18/AnimalHospital18ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.animalhospital18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHospital18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
