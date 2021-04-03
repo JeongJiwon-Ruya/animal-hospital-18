2e26f15025f744099aa6eb0ff8b309b6c94d2541
201702000 <jundol1222@naver.com>
2021-04-04 00:11:54 +0900

    Dog_logback_yml_writed

--- src/main/java/org/cnu/realcoding/animalhospital18/domain/Dog.java
+++ src/main/java/org/cnu/realcoding/animalhospital18/domain/Dog.java
@@ -1,2 +1,12 @@
-package org.cnu.realcoding.animalhospital18.domain;public class Dog {
+package org.cnu.realcoding.animalhospital18.domain;
+
+import java.util.List;
+
+public class Dog {
+    private String name;
+    private String kind;
+    private String ownerName;
+    private String ownerPhoneNumber;
+    private List<String> medicalRecords;
 }
+
